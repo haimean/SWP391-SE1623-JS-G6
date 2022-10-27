e51f113e32199e471bcf3ac28c7e870ac407b795
Pham Hai Minh <haiminh.work@gmail.com>
2022-10-27 07:21:36 +0700

    feat: register

--- src/java/SendEmail/SendEmail.java
+++ src/java/SendEmail/SendEmail.java
@@ -73,48 +73,4 @@ public class SendEmail {
         return test;
     }
 
-    public static void main(String[] args) {
-        String toEmail = "minhhaiphamo1@gmail.com";
-        String fromEmail = "haiminh.work@gmail.com";
-        String password = "pxstclppgvhyhcvk";
-
-        try {
-
-            // your host email smtp server details
-            Properties pr = new Properties();
-            pr.setProperty("mail.smtp.host", "smtp.gmail.com");
-            pr.setProperty("mail.smtp.port", "587");
-            pr.setProperty("mail.smtp.auth", "true");
-            pr.setProperty("mail.smtp.starttls.enable", "true");
-            pr.put("mail.smtp.socketFactory.port", "587");
-            pr.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            
-            //get session to authenticate the host email address and password
-            Session session = Session.getInstance(pr, new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(fromEmail, password);
-                }
-            });
-
-            //set email message details
-            Message mess = new MimeMessage(session);
-
-            //set from email address
-            mess.setFrom(new InternetAddress(fromEmail));
-            //set to email address or destination email address
-            mess.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
-
-            //set email subject
-            mess.setSubject("User Email Verification");
-
-            //set message text
-            mess.setText("Registered successfully.Please verify your account using this code: ");
-            //send the message
-            Transport.send(mess);
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
-}
+ }

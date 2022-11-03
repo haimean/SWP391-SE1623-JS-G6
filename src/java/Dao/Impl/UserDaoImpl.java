dbc6d38fce9d933f7a95233de29be1349877ad11
FnPrtScr <tn4490523@gmail.com>
2022-11-03 20:59:34 +0700

    .fix

--- src/java/Dao/Impl/UserDaoImpl.java
+++ src/java/Dao/Impl/UserDaoImpl.java
@@ -396,4 +396,9 @@ public class UserDaoImpl implements UserDao {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from
                                                                        // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
+    @Override
+    public boolean delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
